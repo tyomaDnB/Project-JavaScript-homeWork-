191a5e0bb0840e0014e8b6b843afac65c29379a8
Artyom <tyoma.dnb@gmail.com>
2025-11-27 20:43:02 +0300

    exercise3

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@ const numberOfFilms = prompt('Сколько фильмов вы уже посм
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: movies = {},
-    actors: actors = {},
-    genres: genres = [],
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', 'name movie'),
+      b = prompt('На сколько оцените его?', 'rating movie'),
+      c = prompt('Один из последних просмотренных фильмов?', 'name movie'),
+      d = prompt('На сколько оцените его?', 'rating movie');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
